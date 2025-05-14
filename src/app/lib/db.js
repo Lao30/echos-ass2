d7e5864a94ce8d2a9c9f2b1f0f89eba96ccff8c6
Nang Lao Pown <nanglaopown@Nangs-MacBook-Air.local>
2025-05-14 15:03:55 +0800

    postgres

---
+++ src/app/lib/db.js
@@ -0,0 +1,16 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+export async function query(text, params) {
+  const result = await pool.query(text, params);
+  return result;
+}
+
+export { pool };
