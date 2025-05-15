d1fcbe0b53075d433974bdaaccae5bf8f1e935bf
Nang Lao Pown <nanglaopown@Nangs-MacBook-Air.local>
2025-05-15 08:53:26 +0800

    sidebar

---
+++ src/app/components/Sidebar.js
@@ -0,0 +1,40 @@
+"use client";
+import React from 'react';
+import '../../app/globals.css';
+
+const organizerMenuItems = [
+  { name: 'Dashboard', icon: '🏠' },
+  { name: 'CreateEvents', icon: '🗓️' },
+  { name: 'Analytical', icon: '📊' },
+  { name: 'Settings', icon: '⚙️' },
+];
+
+const adminMenuItems = [
+  { name: 'UserRegister', icon: '👥' },
+  { name: 'AnalyticalReport', icon: '📊' },
+  { name: 'Settings', icon: '⚙️' },
+];
+
+export default function Sidebar({ selectedPage, setSelectedPage, isAdmin }) {
+  const itemsToDisplay = isAdmin ? adminMenuItems : organizerMenuItems;
+
+  return (
+    <div className="mt-10 ml-5 w-[300px] h-[900px] bg-[#e3e3e3] p-4 shadow-md rounded-4xl justify-center items-center">
+      <div className="text-2xl font-bold mb-6">LOGO</div>
+      {itemsToDisplay.map((item) => (
+        <div
+          key={item.name}
+          onClick={() => setSelectedPage(item.name)}
+          className={`flex items-center p-3 mb-2 cursor-pointer rounded-md text-black ${
+            selectedPage === item.name ? 'bg-[#BF9264] font-bold' : ''
+          }`}
+        >
+          <span className="w-6 h-6 text-sm text-black flex items-center justify-center rounded-full mr-3">
+            {item.icon}
+          </span>
+          {item.name}
+        </div>
+      ))}
+    </div>
+  );
+}
